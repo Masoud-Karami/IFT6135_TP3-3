3bed921229d62324d9db87dad4c177ab3c982f48
AndrewRWilliams <39383769+AndrewRWilliams@users.noreply.github.com>
2019-04-18 18:00:16 -0400

    Update VAE.py

--- VAE.py
+++ VAE.py
@@ -38,7 +38,6 @@ from torch.functional import F
 from torch.optim import Adam
 
 
-
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -48,7 +47,7 @@ batch_size = 128
 learning_rate = 31e-4
 image_size = 784
 h_dim = 400
-z_dim = 20
+z_dim = 100
 num_epochs = 20
 
 
@@ -74,69 +73,58 @@ def get_data_loader(dataset_location, batch_size):
         splitdata.append(dataset_loader)
     return splitdata
 
-train, valid, test = get_data_loader("binarized_mnist", 64)
+train_loader, valid_loader, test_loader = get_data_loader("binarized_mnist", batch_size)
 
 import matplotlib
 import matplotlib.pyplot as plt
 %matplotlib inline
-for x in train:
+for x in train_loader:
     plt.imshow(x[0, 0])
     break
     
 #%%
-
-parser = argparse.ArgumentParser(description='PyTorch VAE MNIST Example')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 2)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-#parser.add_argument('--hidden',type=int,default=10,metavar='N',
-#                    help='number of dimension for z')
-#parser.add_argument('--comp',type=int,default=100,metavar='N',
-#                    help='maximum number of components in DP')
-
-args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-#torch.manual_seed(args.seed)
-
-# DataLoader instances will load tensors directly into GPU memory
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-
-# Download or load downloaded MNIST dataset
-# shuffle data at every epoch
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
-                   transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-
-# Same for test data
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-    batch_size, shuffle=True, **kwargs)
-
-#%%
 class VAE(nn.Module):
-    def __init__(self, image_size=784, h_dim=400, z_dim=20):
+    def __init__(self, image_size=784, h_dim=400, z_dim=100):
         super(VAE, self).__init__()
-        self.fc_1 = nn.Linear(image_size, h_dim)
-        self.fc_2 = nn.Linear(h_dim, z_dim)
-        self.fc_3 = nn.Linear(h_dim, z_dim)
-        self.fc_4 = nn.Linear(z_dim, h_dim)
-        self.fc_5 = nn.Linear(h_dim, image_size)
         
+#         self.encoder = nn.Sequential(nn.Linear(image_size, h_dim),\
+#                                 nn.ReLU()\
+#                                )
+        self.encoder= nn.Sequential(nn.Conv2d(1,32,3),\
+                                    nn.ELU(),\
+                                    nn.AvgPool2d(2,2),\
+                                    nn.Conv2d(32,64,3),\
+                                    nn.ELU(),\
+                                    nn.AvgPool2d(2,2),\
+                                    nn.Conv2d(64,256,5),\
+                                    nn.ELU(),\
+                                    )
+#         self.fc_1 = nn.Linear(image_size, h_dim)
+        self.fc_1 = nn.Linear(256, z_dim)
+        self.fc_2 = nn.Linear(256, z_dim)
+
+        self.fc_3 = nn.Linear(z_dim, 256)
+#         self.fc_5 = nn.Linear(h_dim, image_size)
+        self.decoder= nn.Sequential(nn.ELU(),\
+                                    nn.Conv2d(256,64,5, padding = 4),\
+                                    nn.ELU(),\
+                                    nn.UpsamplingBilinear2d(scale_factor=2),\
+                                    nn.Conv2d(64, 32, 3, padding=2),\
+                                    nn.ELU(),\
+                                    nn.UpsamplingBilinear2d(scale_factor=2),\
+                                    nn.Conv2d(32, 16, 3, padding=2),\
+                                    nn.ELU(),\
+                                    nn.Conv2d(16, 1, 3, padding=2),\
+                                    nn.Sigmoid()
+                                    )
         
     """ Encode a batch of samples, and return posterior parameters for each point."""    
     def encode(self, x):
-        h_1 = F.relu(self.fc_1(x))
-        return self.fc_2(h_1), self.fc_3(h_1)
-    
-    
+#         h_1 = F.relu(self.fc_1(x))
+      h_1 = self.encoder(x)
+      return self.fc_1(h_1.view(-1,256)), self.fc_2(h_1.view(-1,256))
+
+
     """ Reparameterisation trick to sample z values. 
         This is stochastic during training,  and returns the mode during evaluation.
         For each training sample (we get 128 batched at a time)
@@ -163,25 +151,24 @@ class VAE(nn.Module):
         eps = torch.randn_like(std)
         return mu + eps * std
 
-    
+
     """ Decode a batch of latent variables"""
     def decode(self, z):
-        h_3 = F.relu(self.fc_4(z))
-        return F.sigmoid(self.fc_5(h_3))
-    
-    
+    #        h_3 = F.relu(self.fc_4(z))
+    #        return F.sigmoid(self.fc_5(h_3))
+        h_2 = self.fc_3(z).view(-1,256,1,1)
+        h_3 = self.decoder(h_2)
+        return h_3
+
+
     """ Takes a batch of samples, encodes them, and then decodes them again to compare."""
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
+        mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
-    
-
+#%%
 model = VAE().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-#%%
-
 """ ELBO assuming entries of x are binary variables, with closed form KLD."""
 def loss_function(x_reconst, x, mu, logvar):
     bce = F.binary_cross_entropy(x_reconst, x.view(-1, 784), reduction='sum')
@@ -197,7 +184,8 @@ def loss_function(x_reconst, x, mu, logvar):
 def train(epoch):
     model.train()
     train_loss = 0
-    for batch_idx, (data, _) in enumerate(train_loader):
+    log_interval = 100
+    for batch_idx, data in enumerate(train_loader):
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
@@ -205,7 +193,7 @@ def train(epoch):
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        if batch_idx % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\t Loss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset), 100. *
                 batch_idx / len(train_loader), loss.item() / len(data)
@@ -224,40 +212,35 @@ def test(epoch):
     test_loss = 0
     # ind = np.arange(x.shape[0])
     with torch.no_grad():
-        for i, (data, _) in enumerate(test_loader):
+        for i, data in enumerate(valid_loader):
             data = data.to(device)
 #           test_data = torch.from_numpy(test_data[np.random.choice(ind, size=batch_size)])
 #           test_data = Variable(test_data, requires_grad=False)
             reconst_batch, mu, logvar = model(data)
             test_loss += loss_function(reconst_batch, data, mu, logvar).item()
             if i == 0:
-                n = min(data.size(0), 8)
+                n = min(data.size(0), 28)
                 comparison = torch.cat([data[:n],
-                                        reconst_batch.view(args.batch_size, 1, 28, 28)[:n]])
-                save_image(comparison.cpu(),
-                         'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+                                        reconst_batch.view(128, 1, 28, 28)[:n]])
+#                 save_image(comparison.cpu(),
+#                          'results/reconstruction_' + str(epoch) + '.png', nrow=n)
 #            print(data.view(batch_size, 2,2)[:n])
 #            print(reconst_batch.view(batch_size, 2,2)[:n])
             
             
-    test_loss /= len(test_loader.dataset)
+    test_loss /= len(valid_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
         
 #%%    
 if __name__ == "__main__":
-    for epoch in range(1, args.epochs + 1):
+    for epoch in range(1, 21):
         train(epoch)
         test(epoch)
         # 64 sets of random z_dim-float vectors, i.e. 64 locations / MNIST
         # digits in latent space
         with torch.no_grad():
-            sample = torch.randn(64, z_dim).to(device)
+            sample = torch.randn(128, z_dim).to(device)
             sample = model.decode(sample).cpu()
-            save_image(sample.view(64, 1, 28, 28),
-                       'results/sample_' + str(epoch) + '.png')
-    torch.save(model.state_dict(), 'saved_models/vae.pth')            
-            
-            
-            
-            
-            
+#             save_image(sample.view(64, 1, 28, 28),
+#                        'results/sample_' + str(epoch) + '.png')
+    torch.save(model.state_dict(), 'vae.pth')            
